83f619c2f1d9072fd939d8de6f98c1924298fc58
Albina <kraeva_lelya@list.ru>
2021-11-05 16:45:43 +0300

    hw5(basket)

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -35,8 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'authapp',
     'mainapp',
+    'basketapp'
 ]
 
 MIDDLEWARE = [
