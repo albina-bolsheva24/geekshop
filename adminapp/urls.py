179eddbdc88222b2afab59111dd4a5b76efd3052
Albina <kraeva_lelya@list.ru>
2021-11-15 10:27:18 +0300

    hw8

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,7 +5,7 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('user/create/', admin_views.user_create, name='user_create'),
-    path('user/', admin_views.users, name='users_list'),
+    path('user/', admin_views.UserListView.as_view(), name='users_list'),
     path('user/update/<ink:pk>/', admin_views.user_update, name='user_update'),
     path('user/delete/<ink:pk>/', admin_views.user_delete, name='user_delete'),
 
@@ -14,9 +14,9 @@ urlpatterns = [
     path('categories/update/<ink:pk>/', admin_views.category_update, name='category_update'),
     path('categories/delete/<ink:pk>/', admin_views.category_delete, name='category_delete'),
 
-    path('products/create/<int:pk>/', admin_views.product_create, name='product_create'),
-    path('products/', admin_views.products, name='products_list'),
-    path('products/update/<ink:pk>/', admin_views.product_update, name='product_update'),
-    path('products/delete/<ink:pk>/', admin_views.product_delete, name='product_delete'),
-    path('products/detail/<ink:pk>/', admin_views.product_detail, name='product_detail'),
+    path('products/create/<int:pk>/', admin_views.ProductCreateView.as_view(), name='product_create'),
+    path('products/', admin_views.ProductsListView.as_view(), name='products_list'),
+    path('products/update/<ink:pk>/', admin_views.ProductUpdateView.as_view(), name='product_update'),
+    path('products/delete/<ink:pk>/', admin_views.ProductDeleteView.as_view(), name='product_delete'),
+    path('products/detail/<ink:pk>/', admin_views.ProductDetailView.as_view(), name='product_detail'),
 ]
